31058c305f1185048d886104f2126a761309b801
Eugene Brodsky <eugene@teehanlax.com>
2012-10-30 16:11:42 -0400

    now using "Added, Removed, Registered" as status. Also methods to connect and disconnect devices.

--- usbwatchman/tracker/models.py
+++ usbwatchman/tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -13,17 +14,35 @@ class Device(models.Model):
 		return self.name
 		
 	def last_event(self):
-		return self.event_set.latest('timestamp')
+		if self.has_events():
+			return self.event_set.latest()
+		else:
+			return "Device has no events"
+		## what if no events? 
 		
 	def current_state(self):
-		return str(self.event_set.latest('timestamp').event_type) ## This uses the last state change in database to determine current state. Is there a better way?
+		if self.has_events():
+			return str(self.event_set.latest('timestamp').event_type) ## This uses the last state change in database to determine current state. Is there a better way?
+		else:
+			return "Device has no events"
+		## what if no events?
 		
 	def connected(self):
-		return self.current_state() == "Connected"
+		return self.current_state() == "Added"
+		## assumes this is sanitized by current_state()
 		
-	connected.boolean = True
-
+	connected.boolean = True ## to let the template know that it should show as a check/uncheck icon
 	
+	def connect(self):
+		self.event_set.create(event_type='Added', timestamp=timezone.now())
+
+	def disconnect(self):
+		self.event_set.create(event_type='Removed', timestamp=timezone.now())
+	
+
+	def has_events(self):
+		return len(self.event_set.all()) > 0
+
 class Event(models.Model):
 	device = models.ForeignKey(Device)
 	timestamp = models.DateTimeField()
@@ -31,5 +50,7 @@ class Event(models.Model):
 	
 	def __unicode__(self):
 # 		return (self.timestamp.isoformat() + ": " + self.event_type + ": " + self.device.name )
-		return (self.event_type + " " + self.timestamp.isoformat())
-	 
+# 		return (self.event_type + " " + self.timestamp.strftime('%Y/%m/%d %H:%M:%S'))
+		return "Event " + str(self.id)		
+		
+	get_latest_by = 'timestamp'
